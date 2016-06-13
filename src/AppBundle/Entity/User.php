535d8c62c301fd2ada229a4ec66e020ed92e661f
Mikkel Ricky <rimi@aarhus.dk>
2016-06-13 13:51:59 +0200

    Issue #EV-38 by rimi-itk: Added authentication to API

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Dunglas\ApiBundle\Annotation\Iri;
+use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ *
+ * @UniqueEntity("email")
+ * @UniqueEntity("username")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string The username of the author.
+     *
+     * @Groups({"user_read", "user_write"})
+     */
+    protected $username;
+
+    /**
+     * @var string The email of the user.
+     *
+     * @Groups({"user_read", "user_write"})
+     */
+    protected $email;
+
+    /**
+     * @var string Plain password. Used for model validation. Must not be persisted.
+     *
+     * @Groups({"user_write"})
+     */
+    protected $plainPassword;
+
+    /**
+     * @var boolean Shows that the user is enabled
+     *
+     * @Groups({"user_read", "user_write"})
+     */
+    protected $enabled;
+
+    /**
+     * @var array Array, role(s) of the user
+     *
+     * @Groups({"user_read", "user_write"})
+     */
+    protected $roles;
+}
