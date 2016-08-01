70787873b4d36ac51229c2d2af8a6eb7164b3973
turegjorup <ture@ignatz.dk>
2016-08-01 12:21:42 +0200

    EV-56: Fixed FixturesBundle missing in AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,6 +33,7 @@ class AppKernel extends Kernel
       $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
       $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
       $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
+      $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
     }
 
     return $bundles;
