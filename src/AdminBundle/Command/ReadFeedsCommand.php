65c6b7e817293e2b640782035edcffffdf4e2c47
Mikkel Ricky <rimi@aarhus.dk>
2016-08-16 12:22:49 +0200

    Cleaned up code

--- src/AdminBundle/Command/ReadFeedsCommand.php
+++ src/AdminBundle/Command/ReadFeedsCommand.php
@@ -153,38 +153,4 @@ class ReadFeedsCommand extends ContainerAwareCommand implements Controller {
   public function convertValue($value, $name) {
     return $this->converter->convert($value, $name);
   }
-
-  // http://php.net/manual/en/function.parse-url.php#106731
-  private function unparse_url($parsed_url) {
-    $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-    $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-    $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-    $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-    $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
-    $pass     = ($user || $pass) ? "$pass@" : '';
-    $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-    $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-    $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
-    return "$scheme$user$pass$host$port$path$query$fragment";
-  }
-
-  private function parseDate($value) {
-    $date = null;
-    // JSON date (/Date(...)/)
-    if (preg_match('@/Date\(([0-9]+)\)/@', $value, $matches)) {
-      $date = new \DateTime();
-      $date->setTimestamp(((int)$matches[1]) / 1000);
-    } else if (is_numeric($value)) {
-      $date = new \DateTime();
-      $date->setTimestamp($value);
-    }
-
-    if ($date === null) {
-      try {
-        $date = new \DateTime($value);
-      } catch (\Exception $e) {}
-    }
-
-    return $date;
-  }
 }
