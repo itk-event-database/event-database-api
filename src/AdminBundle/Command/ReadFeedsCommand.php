c946d046e1179d85b32abb78c8dd9d16cfeeccf8
Mikkel Ricky <rimi@aarhus.dk>
2016-10-03 14:26:47 +0200

    Added --list option to read feeds command

--- src/AdminBundle/Command/ReadFeedsCommand.php
+++ src/AdminBundle/Command/ReadFeedsCommand.php
@@ -2,21 +2,35 @@
 
 namespace AdminBundle\Command;
 
+use AdminBundle\Entity\Feed;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ReadFeedsCommand extends ContainerAwareCommand {
+  /**
+   * @var OutputInterface
+   */
+  private $output;
+
   protected function configure() {
     $this
       ->setName('events:feeds:read')
       ->setDescription('Read event feeds')
       ->addOption('name', null, InputOption::VALUE_REQUIRED, 'The name of the feed')
-      ->addOption('id', null, InputOption::VALUE_REQUIRED, 'The ID of the feed');
+      ->addOption('id', null, InputOption::VALUE_REQUIRED, 'The ID of the feed')
+      ->addOption('list', null, InputOption::VALUE_NONE, 'List all feeds');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->output = $output;
+
+    if ($input->getOption('list')) {
+      $this->listFeeds();
+      return;
+    }
+
     $name = $input->getOption('name');
     $id = $input->getOption('id');
 
@@ -24,24 +38,48 @@ class ReadFeedsCommand extends ContainerAwareCommand {
     $noOfFeeds = count($feeds);
 
     if ($noOfFeeds == 0) {
-      $output->writeln('No feeds found!');
+      $this->writeln('No feeds found!');
     } else {
-      $output->writeln(sprintf('Reading %s feed%s:', $noOfFeeds, ($noOfFeeds == 1) ? '' : 's'));
+      $this->writeln(sprintf('Reading %s feed%s:', $noOfFeeds, ($noOfFeeds == 1) ? '' : 's'));
     }
 
     $reader = $this->getContainer()->get('feed_reader');
     $reader->setOutput($output);
 
-    foreach ($feeds as $name => $feed) {
-      $output->writeln([
-        str_repeat('-', 80),
-        'feed id: ' . $feed->getId(),
-        'url:     ' . $feed->getUrl(),
-        'user:    ' . $feed->getCreatedBy(),
-        str_repeat('-', 80),
-      ]);
+    foreach ($feeds as $feed) {
+      $this->writeln(str_repeat('-', 80));
+      $this->writeFeedInfo($feed);
+      $this->writeln(str_repeat('-', 80));
 
       $reader->read($feed);
+    }
+  }
+
+  private function listFeeds() {
+    $feeds = $this->getFeeds(null, null);
+    foreach ($feeds as $feed) {
+      $this->writeln(str_repeat('-', 80));
+      $this->writeFeedInfo($feed);
+      $this->writeln(str_repeat('-', 80));
+    }
+  }
+
+  private function writeFeedInfo(Feed $feed) {
+    $this->writeln([
+      'name:   ' . $feed->getName(),
+      'id:     ' . $feed->getId(),
+      'url:    ' . $feed->getUrl(),
+      'user:   ' . $feed->getCreatedBy(),
+    ]);
+  }
+
+  private function writeln($messages) {
+    $this->write($messages, true);
+  }
+
+  private function write($messages, $newline = false) {
+    if ($this->output) {
+      $this->output->write($messages, $newline);
     }
   }
 
