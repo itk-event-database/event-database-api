74420511e451bd116a68ace4c80949547268557d
turegjorup <ture@ignatz.dk>
2016-10-04 14:00:23 +0200

    EV-73: Added Categories fixture

---
+++ src/AdminBundle/DataFixtures/ORM/LoadCategories.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AdminBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Tag;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\Yaml\Yaml;
+
+class LoadCategories extends LoadData
+{
+  protected $order = 2;
+
+  public function load(ObjectManager $manager)
+  {
+    $yaml = $this->loadFixture('categories.yml');
+    $config = Yaml::parse($yaml);
+
+    $tags = array();
+    $tagManager = $this->container->get('fpn_tag.tag_manager');
+
+    foreach ($config['data'] as $name => $configuration) {
+      $tags[] = $tagManager->loadOrCreateTag($configuration['name']);
+    }
+
+    $manager->flush();
+  }
+
+}
