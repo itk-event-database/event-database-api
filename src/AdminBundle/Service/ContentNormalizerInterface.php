ee261d421f4303151446392afb6772c0a7f578ee
Mikkel Ricky <rimi@aarhus.dk>
2016-10-06 17:35:14 +0200

    EV-82: Added tag and content normalizers

---
+++ src/AdminBundle/Service/ContentNormalizerInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AdminBundle\Service;
+
+interface ContentNormalizerInterface {
+  /**
+   * Normalize html content.
+   *
+   * @param string $content
+   *   The html string.
+   *
+   * @return string
+   *   Normalized and cleaned html.
+   */
+  public function normalize(string $content): string;
+}
