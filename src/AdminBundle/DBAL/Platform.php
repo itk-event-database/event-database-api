caec11878a79a50838705be7984d1caf186064af
turegjorup <ture@ignatz.dk>
2016-10-07 13:28:53 +0200

    EV-73: Fixed fixtures:load --purge-with-truncate mysql error

---
+++ src/AdminBundle/DBAL/Platform.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * @TODO: Missing description.
+ */
+
+namespace AdminBundle\DBAL;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+
+/**
+ * Class Platform
+ * @package AppBundle\DBAL
+ *
+ * Problem: On MySQL >= 5.5.7 fixtures bundle cannot truncate tables because of foreign key constraints
+ *
+ * Fixes https://github.com/doctrine/data-fixtures/pull/127
+ * Using https://coderwall.com/p/staybw/workaround-for-1701-cannot-truncate-a-table-referenced-in-a-foreign-key-constraint-using-doctrine-fixtures-load-purge-with-truncate
+ */
+class Platform extends MySqlPlatform {
+  /**
+   * {@inheritdoc}
+   */
+  public function getTruncateTableSQL($tableName, $cascade = FALSE) {
+    return sprintf('SET foreign_key_checks = 0;TRUNCATE %s;SET foreign_key_checks = 1;', $tableName);
+  }
+}
