8bd800899ce665e2a5c167a65ae1c0f15c739a0a
Mikkel Ricky <rimi@aarhus.dk>
2016-10-20 09:50:05 +0200

    EV-87: Added file upload to api

---
+++ src/AdminBundle/Action/UploadAction.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AdminBundle\Action;
+
+use ApiPlatform\Core\Bridge\Symfony\Routing\Router;
+use League\Uri\Modifiers\Resolve;
+use League\Uri\Schemes\Http as HttpUri;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ *
+ */
+class UploadAction {
+  /**
+   * @var Router
+   */
+  private $router;
+
+  /**
+   * @var ContainerInterface
+   */
+  private $container;
+
+  /**
+   * @var Resolve
+   */
+  private $baseUrlResolver;
+
+  /**
+   * @var string
+   */
+  private $uploadsUrl;
+
+  /**
+   * Constructor.
+   *
+   * @param \ApiPlatform\Core\Bridge\Symfony\Routing\Router $router
+   *   The router.
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container.
+   */
+  public function __construct(Router $router, ContainerInterface $container) {
+    $this->router = $router;
+    $this->container = $container;
+
+    $this->baseUrlResolver = new Resolve(HttpUri::createFromString($this->container->getParameter('admin.base_url')));
+    $this->uploadsUrl = rtrim($this->container->getParameter('admin.uploads_url'), '/');
+  }
+
+  /**
+   * @Route("/api/upload", name="api_upload", methods={"POST"})
+   *
+   * Using annotations is not mandatory, XML and YAML configuration files can be used instead.
+   * If you want to decouple your actions from the framework, don't use annotations.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function __invoke(Request $request) {
+    $data = [];
+
+    foreach ($request->files as $file) {
+      $path = $this->container->getParameter('admin.uploads_path');
+      $filename = uniqid($file->getBaseName());
+      $file->move($path, $filename);
+      $data[$file->getBaseName()] = $this->baseUrlResolver->__invoke(HttpUri::createFromString($this->uploadsUrl . '/' . $filename))->__toString();
+    }
+
+    return new JsonResponse($data);
+  }
+
+}
