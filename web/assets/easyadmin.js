eb271a0ff8c2620c5abd8f218e416037e623b970
Mikkel Ricky <rimi@aarhus.dk>
2017-03-09 11:41:18 +0100

    EV-111: Fixed bug with autocomplete field in collection form

--- web/assets/easyadmin.js
+++ web/assets/easyadmin.js
@@ -6,4 +6,9 @@
 		window.confirmCreateRepeatingOccurrences = function(form) {
 				return confirm('Please confirm creation of repeating occurrences');
 		}
+
+		// @see https://github.com/javiereguiluz/EasyAdminBundle/issues/1518#issuecomment-284824895
+		$('.field-collection').on('easyadmin.collection.item-added', function() {
+				createAutoCompleteFields();
+		});
 }(jQuery));
