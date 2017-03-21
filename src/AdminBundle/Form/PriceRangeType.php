c06c9e2bb3b0a3dc81b37c7ff1dc1887dde46d3a
Mikkel Ricky <rimi@aarhus.dk>
2017-03-21 13:06:58 +0100

    EV-133: Updated ui and translations

---
+++ src/AdminBundle/Form/PriceRangeType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ *
+ */
+class PriceRangeType extends AbstractType
+{
+  public function getParent() {
+    return TextType::class;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName() {
+    return 'adminbundle_pricerange';
+  }
+
+  public function getBlockPrefix() {
+    return $this->getName();
+  }
+}
