5d6642505e9dbadc92c5796e5ba10b80f042c56f
Mikkel Ricky <rimi@aarhus.dk>
2017-05-08 21:53:34 +0200

    SUPPORT-1049: Fixed saving of tags

--- src/AdminBundle/EventSubscriber/EasyAdminSubscriber.php
+++ src/AdminBundle/EventSubscriber/EasyAdminSubscriber.php
@@ -25,8 +25,6 @@ class EasyAdminSubscriber implements EventSubscriberInterface {
     return [
       EasyAdminEvents::POST_INITIALIZE => ['addQueryParameters'],
       EasyAdminEvents::PRE_PERSIST => ['setSlug'],
-      EasyAdminEvents::POST_PERSIST => [['saveTags'], ['postPersist']],
-      EasyAdminEvents::POST_UPDATE => [['saveTags'], ['postUpdate']],
     ];
   }
 
@@ -45,44 +43,11 @@ class EasyAdminSubscriber implements EventSubscriberInterface {
     }
   }
 
-  public function postPersist(GenericEvent $event) {
-    $subject = $event->getSubject();
-    $message = $this->translate('%entity_name% created', ['%entity_name%' => $this->translate($this->getEntityName($subject))]);
-    $this->addFlash('info', $message);
-  }
-
-  public function postUpdate(GenericEvent $event) {
-    $subject = $event->getSubject();
-    $message = $this->translate('%entity_name% updated', ['%entity_name%' => $this->translate($this->getEntityName($subject))]);
-    $this->addFlash('info', $message);
-  }
-
-  private function translate($text) {
-    $translator = $this->container->get('translator');
-    return $translator->trans($text);
-  }
-
-  private function getEntityName($entity) {
-    $name = get_class($entity);
-    return preg_replace('/^([a-z]+\\\\)+/i', '', $name);
-  }
-
-  private function addFlash($type, $message) {
-    // $this->container->get('session')->getFlashBag()->add('info', $message);
-  }
-
   public function setSlug(GenericEvent $event) {
     $entity = $event->getSubject();
 
     if (method_exists($entity, 'getName') && method_exists($entity, 'setSlug')) {
       $entity->setSlug($this->slugifier->slugify($entity->getName()));
-    }
-  }
-
-  public function saveTags(GenericEvent $event) {
-    $entity = $event->getSubject();
-    if ($entity instanceof Taggable) {
-      $this->tagManager->saveTagging($entity);
     }
   }
 
