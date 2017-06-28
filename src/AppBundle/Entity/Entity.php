e24c1a5265ae073453eae61e1eede9bb184dfe53
Mikkel Ricky <rimi@aarhus.dk>
2017-06-28 17:15:46 +0200

    EV-171: Improved feed reader to skip events with no changes

--- src/AppBundle/Entity/Entity.php
+++ src/AppBundle/Entity/Entity.php
@@ -6,4 +6,15 @@ namespace AppBundle\Entity;
  *
  */
 abstract class Entity {
+  private $skipImport = FALSE;
+
+  public function setSkipImport($skipImport) {
+    $this->skipImport = $skipImport;
+
+    return $this;
+  }
+
+  public function getSkipImport() {
+    return $this->skipImport;
+  }
 }
