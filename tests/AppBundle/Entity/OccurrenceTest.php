f7cb79312aad354d6c706db181aec612292f848a
Ture Gjørup <ture@ignatz.dk>
2017-10-18 17:07:22 +0200

    DOKK-719: Removed OccurrenceTest

--- tests/AppBundle/Entity/OccurrenceTest.php
+++ tests/AppBundle/Entity/OccurrenceTest.php
@@ -9,25 +9,25 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 class OccurrenceTest extends DatabaseTestCase
 {
 
-    public function testPlace()
-    {
-        $occurrence = new Occurrence();
-        $place = new Place();
-        $occurrence->setPlace($place);
-        $this->em->persist($place);
-        $this->em->persist($occurrence);
-        $this->em->flush();
-
-        $sql = 'select * from occurrence';
-        $stmt = $this->em->getConnection()->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-        $this->assertEquals(1, count($result));
-
-        $sql = 'select * from place';
-        $stmt = $this->em->getConnection()->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-        $this->assertEquals(1, count($result));
-    }
+//    public function testPlace()
+//    {
+//        $occurrence = new Occurrence();
+//        $place = new Place();
+//        $occurrence->setPlace($place);
+//        $this->em->persist($place);
+//        $this->em->persist($occurrence);
+//        $this->em->flush();
+//
+//        $sql = 'select * from occurrence';
+//        $stmt = $this->em->getConnection()->prepare($sql);
+//        $stmt->execute();
+//        $result = $stmt->fetchAll();
+//        $this->assertEquals(1, count($result));
+//
+//        $sql = 'select * from place';
+//        $stmt = $this->em->getConnection()->prepare($sql);
+//        $stmt->execute();
+//        $result = $stmt->fetchAll();
+//        $this->assertEquals(1, count($result));
+//    }
 }
