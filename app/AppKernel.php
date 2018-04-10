9cd90a5d71aac35981f4238b562e85570bfeced8
Mikkel Ricky <rimi@aarhus.dk>
2018-04-10 16:29:24 +0200

    EV-184: Aligned with Symfony 3.4

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,5 +1,6 @@
 <?php
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
@@ -8,40 +9,43 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
-        new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-        new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-        new Symfony\Bundle\TwigBundle\TwigBundle(),
-        new Symfony\Bundle\MonologBundle\MonologBundle(),
-        new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-        new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-        new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-        new Dunglas\ActionBundle\DunglasActionBundle(),
-        new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
-        new Nelmio\CorsBundle\NelmioCorsBundle(),
-        new FOS\UserBundle\FOSUserBundle(),
-        new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
-        new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-        new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-        new FPN\TagBundle\FPNTagBundle(),
-        new ResqueBundle\Resque\ResqueBundle(),
-        new Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle(),
-        new ItkDev\DatabaseBundle\ItkDevDatabaseBundle(),
-        new AppBundle\AppBundle(),
-        new AdminBundle\AdminBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Dunglas\ActionBundle\DunglasActionBundle(),
+            new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new FPN\TagBundle\FPNTagBundle(),
+            new ResqueBundle\Resque\ResqueBundle(),
+            new Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle(),
+            new ItkDev\DatabaseBundle\ItkDevDatabaseBundle(),
+            new AppBundle\AppBundle(),
+            new AdminBundle\AdminBundle(),
 
-        // EasyAdminBundle
-        new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
-        new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
-        new Vich\UploaderBundle\VichUploaderBundle(),
+            // EasyAdminBundle
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test', 'circleci'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            }
         }
 
         return $bundles;
@@ -64,12 +68,18 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+        $loader->load(function (ContainerBuilder $container) {
+            $container->setParameter('container.autowiring.strict_mode', true);
+            $container->setParameter('container.dumper.inline_class_loader', true);
+
+            $container->addObjectResource($this);
+        });
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 
     public function __construct($environment, $debug)
     {
-      // Force the timezone to be UTC.
+        // Force the timezone to be UTC.
         date_default_timezone_set('UTC');
         parent::__construct($environment, $debug);
     }
