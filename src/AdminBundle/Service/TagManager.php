f2ccbce3e2f5bd1551e6e57b50eda505a9f14c74
Ture Gjørup <ture@ignatz.dk>
2020-09-16 12:33:53 +0200

    EV-216: Refactored tag handling for feed imports

--- src/AdminBundle/Service/TagManager.php
+++ src/AdminBundle/Service/TagManager.php
@@ -11,19 +11,29 @@
 namespace AdminBundle\Service;
 
 use AppBundle\Entity\CustomTaggable;
+use Doctrine\DBAL\DBALException;
 use DoctrineExtensions\Taggable\Taggable;
 use FPN\TagBundle\Entity\TagManager as BaseTagManager;
 
 class TagManager extends BaseTagManager
 {
-    /**
-     * @var TagNormalizerInterface
-     */
+    /** @var TagNormalizerInterface */
     private $tagNormalizer;
 
+    /** @var TagManager */
+    private $unknownTagManager;
+
+    /**
+     * @param TagNormalizerInterface|null $tagNormalizer
+     */
     public function setTagNormalizer(TagNormalizerInterface $tagNormalizer = null)
     {
         $this->tagNormalizer = $tagNormalizer;
+    }
+
+    public function setUnknownTagManager(TagManager $tagManager = null)
+    {
+        $this->unknownTagManager = $tagManager;
     }
 
     /**
@@ -34,7 +44,13 @@ class TagManager extends BaseTagManager
         if ($this->tagNormalizer) {
             $names = $this->tagNormalizer->normalize($names, $this);
         }
+
+        // Remove falsy values
         $names = array_filter($names);
+
+        if ($this->unknownTagManager) {
+            $this->createUnknownTags($names);
+        }
 
         return parent::loadOrCreateTags($names);
     }
@@ -94,5 +110,37 @@ class TagManager extends BaseTagManager
         $this->em->flush();
 
         return true;
+    }
+
+    /**
+     * Create all unknown tags.
+     *
+     * The EventDatabase has a concept og 'known' (i.e. official or approved) tags. If imported
+     * events has tags that are not known they should be created as an 'UnknownTag'.
+     *
+     * @param array $names
+     *   A normalized list of tag names
+     *
+     * @throws DBALException
+     */
+    private function createUnknownTags(array $names): void
+    {
+        $conn = $this->em->getConnection();
+        $sql = 'SELECT EXISTS(SELECT * FROM tag WHERE name = :name) OR EXISTS(SELECT * FROM unknown_tag WHERE name = :name) as tagExists';
+        $stmt = $conn->prepare($sql);
+
+        $unknownTagNames = [];
+        foreach ($names as $name) {
+            $stmt->execute(array('name' => $name));
+            $result = $stmt->fetch();
+
+            if ($result['tagExists'] === '0') {
+                $unknownTagNames[] = $name;
+            }
+        }
+
+        if (!empty($unknownTagNames)) {
+            $this->unknownTagManager->loadOrCreateTags($unknownTagNames);
+        }
     }
 }
