46d4e706db1a5c5062bee2aeede96806adddbcb8
Ture Gjørup <ture@ignatz.dk>
2023-03-24 10:07:15 +0100

    EV-239: Fix coding standards

--- app/DoctrineMigrations/Version20230323133448.php
+++ app/DoctrineMigrations/Version20230323133448.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Eventbase API.
+ *
+ * (c) 2017–2018 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace Application\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,15 +20,15 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20230323133448 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE event__partner_organizers (event_id INT NOT NULL, organizer_id INT NOT NULL, INDEX IDX_9FF4084171F7E88B (event_id), INDEX IDX_9FF40841876C4DDA (organizer_id), PRIMARY KEY(event_id, organizer_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event__partner_organizers ADD CONSTRAINT FK_9FF4084171F7E88B FOREIGN KEY (event_id) REFERENCES event (id)');
@@ -31,10 +39,10 @@ final class Version20230323133448 extends AbstractMigration
         $this->addSql('CREATE UNIQUE INDEX url_soft_unique ON organizer (url, deleted_at)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE event__partner_organizers');
         $this->addSql('DROP INDEX name_soft_unique ON organizer');
